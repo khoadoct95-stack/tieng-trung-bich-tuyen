a02e90e8cf23e24155f5b47625629ffb6611af7e
Ten Cua Ban <email-cua-ban@gmail.com>
2026-08-15 19:57:06 +0700

    Them STATIC_ROOT vao settings

--- bichtuyen_web/settings.py
+++ bichtuyen_web/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Dòng này giúp Django biết thư mục 'static' nằm ở đâu
 STATICFILES_DIRS = [

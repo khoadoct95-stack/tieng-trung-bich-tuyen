3d02243be281ef46ade3bed1ac9d20304f522fb8
Ten Cua Ban <email-cua-ban@gmail.com>
2026-08-20 09:10:49 +0700

    bxh 2

--- courses/urls.py
+++ courses/urls.py
@@ -10,22 +10,22 @@ urlpatterns = [
     path('lesson/<int:lesson_id>/quiz-1/', views.quiz_1_view, name='quiz_1'),
     path('lesson/<int:lesson_id>/quiz-2/', views.quiz_2_view, name='quiz_2'),
     path('lesson/<int:lesson_id>/quiz-3/', views.quiz_3_view, name='quiz_3'),
-    # Thêm dòng này cho Game 4:
     path('lesson/<int:lesson_id>/quiz-4/', views.quiz_4_view, name='quiz_4'),
-    # Thêm dòng này:
     path('lesson/<int:lesson_id>/games/', views.game_zone_view, name='game_zone'),
+    
     path('register/', views.register_view, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='courses/login.html'), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('save-score/', views.save_score, name='save_score'),
-    # Thêm dòng này:
-    path('dashboard/', views.dashboard_view, name='dashboard'),
-    # Thêm 2 dòng này cho Hồ sơ cá nhân:
+    
+    # Đã đổi tên đường dẫn này thành 'leaderboard' để khớp với thẻ <a href="{% url 'leaderboard' %}">
+    path('leaderboard/', views.dashboard_view, name='leaderboard'),
+    
     path('profile/', views.profile_view, name='profile'),
     path('github_webhook/', views.github_webhook, name='github_webhook'),
+    
     path('exams/', views.exam_list, name='exam_list'),
     path('exam/<int:exam_id>/', views.take_exam, name='take_exam'),
     path('exam-result/<int:result_id>/', views.exam_result, name='exam_result'),
     path('exam-review/<int:result_id>/', views.review_exam, name='review_exam'),
-    path('dashboard/', views.student_dashboard, name='dashboard'),
 ]

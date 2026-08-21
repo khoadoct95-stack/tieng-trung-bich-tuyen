eb487e2fbc50c50f845bf074e59f5f4eb68a1a41
Ten Cua Ban <email-cua-ban@gmail.com>
2026-08-21 22:38:40 +0700

    fix2

--- courses/urls.py
+++ courses/urls.py
@@ -28,5 +28,6 @@ urlpatterns = [
     path('exam/<int:exam_id>/', views.take_exam, name='take_exam'),
     path('exam-result/<int:result_id>/', views.exam_result, name='exam_result'),
     path('exam-review/<int:result_id>/', views.review_exam, name='review_exam'),
+    path('import-excel/', views.import_excel, name='import_excel'),
     path('upload-zip/', views.upload_exam_images_zip, name='upload_exam_images_zip'),
 ]
